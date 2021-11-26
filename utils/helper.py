15c0b3187192697e038aaf1fb6b88df4eb60eb22
satyanekkanti <satya.nekkanti@duranc.com>
2021-11-27 01:49:18 +0530

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -1,6 +1,8 @@
 ## Helper functions
 
 import torch
+import random
+import numpy as np
 from torchsummary import summary
 
 def get_device():
@@ -26,3 +28,18 @@ def model_summary(model, input_size):
     Summary of the model.
     """
     summary(model, input_size=input_size)
+
+
+
+def seed_everything(seed: int):
+    """
+    Seed everything for reproducibility and deterministic behaviour.
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
