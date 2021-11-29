d4a6255ab2efa52f2738b92de83070d6c6d5cda4
satyanekkanti <satya.nekkanti@duranc.com>
2021-11-29 07:25:34 +0530

    Update custom_resnet.py

--- models/custom_resnet.py
+++ models/custom_resnet.py
@@ -1,82 +1,104 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class BasicBlock(nn.Module):
+    """
+    Basic block of the ResNet.
+    """
+
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1, resblock=True):
-        super(BasicBlock, self).__init__()
+    def __init__(self, in_planes, planes, stride=1):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
 
-        self.has_resblock = resblock
-
-        self.conv = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.mp = nn.MaxPool2d(2, 2)
-        self.bn = nn.BatchNorm2d(planes)
-
-        if self.has_resblock:
-            self.conv1 = nn.Conv2d(
-                planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-            self.bn1 = nn.BatchNorm2d(planes)
-            self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                                   stride=1, padding=1, bias=False)
-            self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
 
     def forward(self, x):
-        x = F.relu(self.bn(self.mp(self.conv(x))))
-        if self.has_resblock:
-            out = F.relu(self.bn1(self.conv1(x)))
-            out = F.relu(self.bn2(self.conv2(out)))
-            r1 = out + x  # This is the residual block
-            return x + r1
-        else:
-            return x
+        """
+        Forward method.
+        """
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out = F.relu(out)
+        return out
 
 
-class CustomResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
-        super(CustomResNet, self).__init__()
-        self.in_planes = 64
+## Assignment Based Custom network
+class CustomResNetModule(nn.Module):
+    """
+    ResNet Architecture.
+    """
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-                               stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+    def __init__(self, block, num_classes=10):
+        super().__init__()
 
-        self.layer1 = self._make_layer(block, 128, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 256, num_blocks[1], stride=1, resblock=False)
-        self.layer3 = self._make_layer(block, 512, num_blocks[2], stride=1)
-        self.pool   = nn.MaxPool2d(4)
+        # PrepLayer - Conv 3x3 s1, p1) >> BN >> RELU [64k]
+        self.prepLayer = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1, stride=1),  
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+        )
 
-        self.linear1 = nn.Linear(512 * block.expansion, 1024)
-        self.do1 = nn.Dropout(0.1) # Using Dropout
-        self.linear2 = nn.Linear(1024, num_classes)
+        # Conv1Layer X = Conv 3x3 (s1, p1) >> MaxPool2D >> BN >> RELU [128k]
+        self.Conv1Layer = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1, stride=1),
+            nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+        )
 
-    def _make_layer(self, block, planes, num_blocks, stride, resblock=True):
-        strides = [stride] + [1] * (num_blocks - 1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride, resblock))
-            self.in_planes = planes * block.expansion
-        return nn.Sequential(*layers)
+        # ResBlock1( (Conv-BN-ReLU-Conv-BN-ReLU))(X) [128k] 
+        self.ResBlock1 = block(128, 128)
+
+        # Conv2Layer X = Conv 3x3 (s1,p1) >> MaxPooling2D >> BN >> ReLU [256k]
+        self.Conv2Layer = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1, stride=1),
+            nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+        )
+
+        # Conv3Layer X = Conv 3x3 (s1, p1) >> MaxPool2D >> BN >> RELU [512k]
+        self.Conv3Layer = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1, stride=1),
+            nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+        )
+
+        # ResBlock2 ( (Conv-BN-ReLU-Conv-BN-ReLU))(X) [512k]
+        self.ResBlock2 = block(512, 512, norm_type=norm_type)
+
+        # MaxPooling with Kernel Size 4
+        self.pooling = nn.MaxPool2d(4, 4)
+
+        # Fully Connected Layer
+        self.fc1 = nn.Linear(512, num_classes)
 
     def forward(self, x):
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = F.relu(out)
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.pool(out)
+        """
+        Forward method.
+        """
+        X = self.prep(x)
+        X = self.Conv1Layer(X)
+        R1 = self.ResBlock1(X)
+        X = X + R1
+        X = self.Conv2Layer(X)
+        X = self.Conv3Layer(X)
+        R2 = self.ResBlock2(X)
+        X = X + R2
+        X = self.pooling(X)
+        X = X.view(X.size(0), -1)
+        X = self.fc1(X)
+        return X
 
-        out = out.view(out.size(0), -1)
-        out = self.linear1(out)
-        out = F.relu(out)
-        out = self.linear2(out)
 
-        
-        return F.softmax(out, dim=-1)
-
-
-def customResnet():
-    return CustomResNet(BasicBlock, [1, 1, 1])
+def CustomResNet():
+    """
+    Custom ResNet model.
+    """
+    return CustomResNetModule(BasicBlock)
