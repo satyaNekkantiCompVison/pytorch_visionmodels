35d7ac6539e0835a6c0ec35300b45f62e81e856d
satyanekkanti <satya.nekkanti@duranc.com>
2021-11-29 12:42:15 +0530

    Update plot_images.py

--- utils/plot_images.py
+++ utils/plot_images.py
@@ -5,21 +5,42 @@ import torch
 
 
 def plot_axis(train_loss=None, test_loss=None, train_accur = None, test_accur=None):
-	fig, axs = plt.subplots(1,2,figsize=(18,8))
-	axs[0].set_title('LOSS')
-	axs[0].plot(train_loss, label='Train')
-	axs[0].plot(test_loss, label='Test')
-	axs[0].legend()
-	axs[0].grid()
+    fig, axs = plt.subplots(1,2,figsize=(18,8))
+    axs[0].set_title('LOSS')
+    axs[0].plot(train_loss, label='Train')
+    axs[0].plot(test_loss, label='Test')
+    axs[0].legend()
+    axs[0].grid()
 
-	axs[1].set_title('Accuracy')
-	axs[1].plot(train_accur, label='Train')
-	axs[1].plot(test_accur, label='Test')
-	axs[1].legend()
-	axs[1].grid()
+    axs[1].set_title('Accuracy')
+    axs[1].plot(train_accur, label='Train')
+    axs[1].plot(test_accur, label='Test')
+    axs[1].legend()
+    axs[1].grid()
 
-	plt.show()
+    plt.show()
 
+def plot_Lossaxis(train_loss=None, test_loss=None):
+    fig, axs = plt.subplots(1,2,figsize=(18,8))
+    axs[0].set_title('Train-Loss')
+    axs[0].plot(train_loss, label='Train')
+    axs[0].legend()
+    axs[0].grid()
+
+    axs[1].set_title('Test-Loss')
+    axs[1].plot(test_loss, label='Test')
+    axs[1].legend()
+    axs[1].grid()
+
+    plt.show()
+ 
+def plot_Accaxis(train_acc=None, test_Acc=None):
+  fig, axs = plt.subplots()
+  axs.set_title('Accuracy')
+  axs.plot(train_acc, label='Train')
+  axs.plot(test_Acc, label='Test')
+  axs.legend()
+  axs.grid()
 
 def misclassified_images(model, test_loader, device, class_names=None, n_images=20):
     """
